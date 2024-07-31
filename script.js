697ff2601b7f044901cedc340f5be377b2e24a08
sakthivel155 <sakthivelramesh155@gmail.com>
2024-07-31 14:49:00 +0530

    completed

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+// hover show feature template
+const templateContent = {
+    'Contract Templates': {
+        header: 'Featured Contract Templates',
+        cards: [
+            'Free Contract Maker',
+            'Social Media Management Contract Template',
+            'Graphic Design Contract Template',
+            'Digital Marketing Contract Template'
+        ]
+    },
+    'Proposal Templates': {
+        header: 'Featured Proposal Templates',
+        cards: [
+            'Graphic Design Proposal Template',
+            'Business Proposal Template',
+            'Brand Ambassador Proposal Template',
+            'Data Entry Proposal Letter Template'
+        ]
+    },
+    'Invoice Templates': {
+        header: 'Featured Invoice Templates',
+        cards: [
+            'Social Media Invoice Templates',
+            'Contractor Invoice Template',
+            'Web Design Invoice Template',
+            'Influencer Invoice Template'
+        ]
+    },
+    'Agreement Templates': {
+        header: 'Featured Agreement Templates',
+        cards: [
+            'Influencer Agreement Template',
+            'Influencer Collaboration Agreement Template',
+            'Monthly Retainer Agreement Template',
+            'Writer Agreement Template'
+        ]
+    },
+    'Quote Templates': {
+        header: 'Featured Quote Templates',
+        cards: [
+            'Web Design Quotation Template',
+            'Interior Design Quotation Template',
+            'Digital Marketing Quotation Template',
+            'Video Production Quotation Template'
+        ]
+    },
+    'Scope of Work Templates': {
+        header: 'Featured Scope of Work Templates',
+        cards: [
+            'Website Development Scope of Work Template',
+            'Digital Marketing Scope of Work Template',
+            'Consultant Scope of Work Template',
+            'Interior Design Scope of Work Template'
+        ]
+    },
+    'Brief Templates': {
+        header: 'Featured Brief Templates',
+        cards: [
+            'Design Brief Template',
+            'Architecture Design Brief',
+            'Fashion Design Brief',
+            'Creative Brief Template'
+        ]
+    }
+};
+function generateHTML(content) {
+    const cardsHTML = content.cards.map(card => `<div class="templete-card">${card}</div>`).join('');
+    return `
+        <div class="templete-right-content-header">${content.header}</div>
+        <div>${cardsHTML}</div>
+        ${content.header !== 'Featured Brief Templates' ? '<div class="show-more">See All Templates</div>' : ''}
+    `;
+}
+
 // Select all items with .list-item and .dropdown-item classes
 const items = document.querySelectorAll('.list-item, .dropdown-item');
 
@@ -73,3 +148,29 @@ function sortAddOns(isAscending = true) {
 
   addOnsCards.forEach(card => addOnsContainer.appendChild(card));
 }
+
+
+// when hover  the dropdown-nav-link show the right-chevron 
+document.addEventListener('DOMContentLoaded', function() {
+    const listItems = document.querySelectorAll('.dropdown-templetes-list .template-list');
+    
+    listItems.forEach(item => {
+        const chevron = item.querySelector('.templete-right-chevron');
+        const templateName = item.textContent.trim();
+        const featureTemplate = document.getElementById('featureTemplates');
+        item.addEventListener('mouseenter', () => {
+            chevron.classList.add('visible');
+            if (templateContent.hasOwnProperty(templateName)) {
+                featureTemplate.innerHTML = generateHTML(templateContent[templateName]);
+            }
+        });
+        
+        
+     
+        
+        item.addEventListener('mouseleave', () => {
+            chevron.classList.remove('visible');
+        });
+    });
+});
+
